827c3d095309e9ef2de3f8ef724b781bbd0dc4d9
MarieJager <marie.j.jager@gmail.com>
2022-01-01 14:25:17 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Live Auction';
+  title = 'Broccoli - live auction for leftovers';
 }
